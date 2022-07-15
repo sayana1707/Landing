56fbf89af5a52dde6e5a08504f012eb3595b9c1f
Sayana Nimaeva <sayana_nimaeva@mail.ru>
2022-07-15 21:23:23 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function buildStyles() {
+    return gulp.src('./scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
+  };
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+  gulp.watch('./scss/**/*.scss', buildStyles);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+}
+  
+  exports.buildStyles = buildStyles;
+  exports.watch = watch;
+
